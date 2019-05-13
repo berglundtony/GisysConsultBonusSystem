599dc3a85ef340b78e0c2e57cdce5a7e9a354e5c
Tony Berglund <tony_berglund@outlook.com>
2019-05-13 09:10:44 +0200

    Add files via upload

---
+++ Angular/src/app/consult-details/consult-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consult-details',
+  templateUrl: './consult-details.component.html',
+  styleUrls: ['./consult-details.component.css']
+})
+export class ConsultDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
